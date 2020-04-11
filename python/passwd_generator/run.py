9154dd563b05a68fc7fd2d074b7c1ba34fcb6069
William Astorga Cárdenas <wastorga@altus.co.cr>
2020-04-10 18:16:39 -0600

    Update to use classes [WIP]
    - Add Option classes and sub-classes
    - Add Constraint class and PasswordGenerator
    - Enable pyperclip

--- python/passwd_generator/run.py
+++ python/passwd_generator/run.py
@@ -4,7 +4,7 @@ import string
 import secrets
 import itertools
 import random
-# import pyperclip
+import pyperclip
 
 """ Types """
 T = typing.T  # Can be anything
@@ -37,6 +37,33 @@ def emptyGen() -> Iterable[T]:
 
 def emptySeq(seqType : Optional[Type] = None) -> Iterable[T]:
     return emptyGen() if seqType is None else seqType()
+
+
+class Options(object):
+    @staticmethod
+    def to_uint(value : T, default: int) -> int:
+        default = default if default > 0 else 0
+        try:
+            result = int(value)
+            result = result
+        except TypeError:
+            result = default
+        finally:
+            return result if result > 0 else default
+
+    @staticmethod
+    def to_bool(value : T) -> bool:
+        return 'true' == value.lower() if isinstance(value, str) else bool(value)
+
+class AlphabetOptions(Options):
+    def __init__(self, **kwargs : Dict[str, T]):
+        self.CASE_SENSITIVE = Options.to_bool(kwargs.get('CASE_SENSITIVE', False))
+        self.INCLUDE_LOWER_CASE = Options.to_bool(kwargs.get('INCLUDE_LOWER_CASE', False))
+        self.INCLUDE_UPPER_CASE = Options.to_bool(kwargs.get('INCLUDE_UPPER_CASE', False))
+        self.INCLUDE_DIGITS = Options.to_bool(kwargs.get('INCLUDE_DIGITS', False))
+        self.INCLUDE_OTHERS = Options.to_bool(kwargs.get('INCLUDE_OTHERS', False))
+        self.INCLUDE_VOWELS = Options.to_bool(kwargs.get('INCLUDE_VOWELS', False))
+        self.INCLUDE_CONSONANTS = Options.to_bool(kwargs.get('INCLUDE_CONSONANTS', False))
 
 
 class Alphabet(object):
@@ -72,32 +99,6 @@ class Alphabet(object):
         for it in iterables:
             for element in it:
                 yield element
-
-
-class Options(object):
-    @staticmethod
-    def to_int(value : T, default: int) -> int:
-        try:
-            result = int(value)
-        except TypeError:
-            result = default
-        finally:
-            return result
-
-
-    @staticmethod
-    def to_bool(value : T) -> bool:
-        return 'true' == value.lower() if isinstance(value, str) else bool(value)
-
-class AlphabetOptions(Options):
-    def __init__(self, **kwargs : Dict[str, T]):
-        self.CASE_SENSITIVE = Options.to_bool(kwargs.get('CASE_SENSITIVE', False))
-        self.INCLUDE_LOWER_CASE = Options.to_bool(kwargs.get('INCLUDE_LOWER_CASE', False))
-        self.INCLUDE_UPPER_CASE = Options.to_bool(kwargs.get('INCLUDE_UPPER_CASE', False))
-        self.INCLUDE_DIGITS = Options.to_bool(kwargs.get('INCLUDE_DIGITS', False))
-        self.INCLUDE_OTHERS = Options.to_bool(kwargs.get('INCLUDE_OTHERS', False))
-        self.INCLUDE_VOWELS = Options.to_bool(kwargs.get('INCLUDE_VOWELS', False))
-        self.INCLUDE_CONSONANTS = Options.to_bool(kwargs.get('INCLUDE_CONSONANTS', False))
 
 
 class Alphabet(object):
@@ -192,26 +193,30 @@ class PasswordConstraints(Options):
     @staticmethod
     def validate_pass_len(pass_min : int, pass_max: int) -> NoReturn:
         if 0 > pass_min or pass_min > pass_max:
-            raise Exception('Values must meet: 0 <= PASS_MIN <= PASS_MAX')
-
-    def __init__(self, **kwargs : Dict[str, T]):
-        self.PASS_MIN = Options.to_int(kwargs.get('PASS_MIN', 0), 0)
-        self.PASS_MAX = Options.to_int(kwargs.get('PASS_MAX', 0), 0)
-        self.validate_pass_len(self.PASS_MIN, self.PASS_MAX)
-        self.LETTERS_MIN = Options.to_int(kwargs.get('LETTERS_MIN', 0), 0)
-        self.DIGITS_MIN = Options.to_int(kwargs.get('DIGITS_MIN', 0), 0)
-        self.NO_NEXT_MATCH = Options.to_bool(kwargs.get('NO_NEXT_MATCH', False))
-        self.PASS_LEN = random.choice(range(self.PASS_MIN, self.PASS_MAX)) if self.PASS_MIN != self.PASS_MAX else self.PASS_MIN
+            raise Exception('Values must meet: 0 <= min <= max')
 
     @staticmethod
-    def countUntil(items: Iterable[T], limit: int) -> (Dict, T, bool):
-        dict_r = {}
-        for i in items:
-            counter = dict_r.get(i, 0) + 1
-            dict_r[i] = counter
-            if counter == limit:
-                return dict_r, i, True
-        return dict_r, i, False
+    def validate_group(group_min : int, group_max: int, chars_max: int, non_group: int) -> NoReturn:
+        if 0 > group_min or group_min > group_max or group_max+non_group > chars_max:
+            raise Exception('Values must meet: 0 <= min <= max and max+others <= max_chars')
+
+    def __init__(self, **kwargs : Dict[str, T]):
+        self.PASS_MIN = Options.to_uint(kwargs.get('PASS_MIN'), 0)
+        self.PASS_MAX = Options.to_uint(kwargs.get('PASS_MAX'), self.PASS_MIN)
+        self.validate_pass_len(self.PASS_MIN, self.PASS_MAX)
+        self.PASS_LEN = random.choice(range(self.PASS_MIN, self.PASS_MAX)) if self.PASS_MIN != self.PASS_MAX else self.PASS_MIN
+
+        self.LETTERS_MIN = Options.to_uint(kwargs.get('LETTERS_MIN'), 0)
+        self.LETTERS_MAX = Options.to_uint(kwargs.get('LETTERS_MAX'), self.LETTERS_MIN)  #LETTERS_MIN
+        self.DIGITS_MIN = Options.to_uint(kwargs.get('DIGITS_MIN'), 0)
+        self.DIGITS_MAX = Options.to_uint(kwargs.get('DIGITS_MAX'), self.DIGITS_MIN)  #DIGITS_MIN
+        self.OTHERS_MIN = Options.to_uint(kwargs.get('OTHERS_MIN'), 0)
+        self.OTHERS_MAX = Options.to_uint(kwargs.get('OTHERS_MAX'), self.OTHERS_MIN)  #OTHERS_MIN
+
+        self.validate_group(self.LETTERS_MIN, self.LETTERS_MAX, self.PASS_MAX, self.DIGITS_MAX + self.OTHERS_MAX)
+        self.validate_group(self.DIGITS_MIN, self.DIGITS_MAX, self.PASS_MAX, self.LETTERS_MAX + self.OTHERS_MAX)
+        self.validate_group(self.OTHERS_MIN, self.OTHERS_MAX, self.PASS_MAX, self.LETTERS_MAX + self.DIGITS_MAX)
+        self.NO_NEXT_MATCH = Options.to_bool(kwargs.get('NO_NEXT_MATCH', False))
 
     @staticmethod
     def pairwise(iterable: Iterable[T]) -> Iterable[Tuple[T]]:
@@ -223,20 +228,6 @@ class PasswordConstraints(Options):
     @staticmethod
     def no_next_match(password: Chars) -> bool:
         return not any(a == b for a,b in PasswordConstraints.pairwise(password))
-
-    @staticmethod
-    def countConsecutiveUntil(items: Iterable[T], limit: int) -> (Dict, T, bool):
-        dict_r, count, item, found = {}, 1, None, False
-        for a, b in PasswordConstraints.pairwise(items):
-            if a == b:
-                count += 1
-            else:
-                dict_r[a] = count
-                count = 1
-            if count == limit:
-                dict_r[a], item, found = count, a, True
-                break
-        return dict_r, item, found
 
     @staticmethod
     def isDigit(value: str) -> bool:
@@ -258,64 +249,113 @@ class PasswordConstraints(Options):
     def isOther(value: str) -> bool:
         return not value.isalnum() and not value.isspace()
 
-    @property
-    def get_constraints(self) -> Iterable[Callable[Chars, bool]]:
+    @staticmethod
+    def countGroup(value: str, dict_r: Dict[str, int]) -> Dict[str, int]:
+        if PasswordConstraints.isLetter(value):
+            dict_r['letters'] = dict_r.get('letters', 0) + 1
+            dict_r['lowers'] = dict_r.get('lowers', 0) + int(PasswordConstraints.isLower(value))
+            dict_r['uppers'] = dict_r.get('uppers', 0) + int(PasswordConstraints.isUpper(value))
+        else:
+            dict_r['digits'] = dict_r.get('digits', 0) + int(PasswordConstraints.isDigit(value))
+            dict_r['others'] = dict_r.get('others', 0) + int(PasswordConstraints.isOther(value))
+        return dict_r
+
+    @staticmethod
+    def countGroups(items: Chars) -> Dict[str, int]:
+        dict_r = {}
+        for i in items:
+            dict_r = PasswordConstraints.countGroup(i, dict_r)
+        return dict_r
+
+    @staticmethod
+    def group_between_min_max(vmin: int, v: int, vmax: int) -> bool:
+        return (vmin <= v) if vmin == vmax else (vmin <= v <= vmax)
+
+    def check(self, passwd: str) -> Iterable[bool]:
+        countedGroups = PasswordConstraints.countGroups(passwd)
         return [
-            lambda passwd: sum(c.isdigit() for c in passwd) >= self.DIGITS_MIN,
-            lambda passwd: sum(c.isalpha() for c in passwd) >= self.LETTERS_MIN,
-            lambda passwd: PasswordConstraints.no_next_match(passwd) if self.NO_NEXT_MATCH else True,
+            PasswordConstraints.no_next_match(passwd) if self.NO_NEXT_MATCH else True,
+            PasswordConstraints.group_between_min_max(self.DIGITS_MIN, countedGroups.get('digits', 0), self.DIGITS_MAX),
+            PasswordConstraints.group_between_min_max(self.LETTERS_MIN, countedGroups.get('letters', 0), self.LETTERS_MAX),
+            PasswordConstraints.group_between_min_max(self.OTHERS_MIN, countedGroups.get('others', 0), self.OTHERS_MAX),
         ]
 
 
 class PasswordGeneratorOptions(Options):
     def __init__(self, **kwargs : Dict[str, T]):
-        self.SECONDS_IN_CLIPBOARD = Options.to_int(kwargs.get('SECONDS_IN_CLIPBOARD', 10), 10)
-        self.MAX_RETRY = Options.to_int(kwargs.get('MAX_RETRY', 10), 10)
+        self.SECONDS_IN_CLIPBOARD = Options.to_uint(kwargs.get('SECONDS_IN_CLIPBOARD'), 10)
+        self.MAX_RETRY = Options.to_uint(kwargs.get('MAX_RETRY'), 100)
 
 
 class PasswordGenerator(object):
+    seqType = StrFromIterable
+
     @classmethod
     def from_dicts(cls, gen_opts: Dict[str, T], opts : Dict[str, T]):
         pass_gen = cls(PasswordGeneratorOptions(**gen_opts), PasswordConstraints(**opts))
         return pass_gen
 
-    def __init__(self, gen_opts: PasswordGeneratorOptions, opts : PasswordConstraints):
+    def __init__(self, gen_opts: PasswordGeneratorOptions, opts : PasswordConstraints, alphabet: Chars):
         self.gen_opts = gen_opts
         self.opts = opts
+        self.alphabet = alphabet
+        self.password = None
+        self.valid = False
 
-    
+    def generate(self, seqType : Optional[Type] = seqType) -> (Iterable[T], bool):
+        i = 0
+        while True:
+            if seqType is None:
+                password = (secrets.choice(self.alphabet) for i in range(self.opts.PASS_LEN))
+            else:
+                password = seqType(secrets.choice(self.alphabet) for i in range(self.opts.PASS_LEN))
+            valid = all(self.opts.check(password))
+            i += 1
+            if valid or i == self.gen_opts.MAX_RETRY:
+                break
+        self.valid = valid
+        self.password = password
+        return password, valid
+
+    def deliver(self) -> NoReturn:
+        if not self.valid:
+            raise Exception('Sorry, could not find any valid password for your options.')
+        else:
+            try:
+                pyperclip.copy(self.password)
+                capture = pyperclip.paste()  # Capture paste and begin countdown
+                try:
+                    print('Password generated and copied to clipboard')
+                    pyperclip.waitForNewPaste(self.gen_opts.SECONDS_IN_CLIPBOARD)
+                except:
+                    pyperclip.copy('')
+                    pass
+            except:
+                print('Password generated:\n{}'.format(self.password))
+            finally:
+                self.password = ''
+                self.valid = False
+                print('--Password-Generator end---')
 
 
-mine = Alphabet.from_dict(CASE_SENSITIVE = True, INCLUDE_UPPER_CASE = True, INCLUDE_DIGITS = True, INCLUDE_CONSONANTS = True)
+mine = Alphabet.from_dict(CASE_SENSITIVE = True, INCLUDE_UPPER_CASE = True, INCLUDE_DIGITS = True, INCLUDE_CONSONANTS = True, INCLUDE_OTHERS = True)
 alphabet = mine.sub_alphabet
+print(alphabet)
 
+passwd_gen = PasswordGenerator(PasswordGeneratorOptions(),
+                               PasswordConstraints(PASS_MIN=8, PASS_MAX=16,
+                                                   LETTERS_MIN=4,
+                                                   DIGITS_MIN=4,
+                                                   OTHERS_MIN=0,
+                                                   NO_NEXT_MATCH = True),
+                               alphabet)
+passwd_gen.generate()
+passwd_gen.deliver()
 
-while True:
-    
-    password = ''.join(secrets.choice(alphabet) for i in range(PASS_LEN))
-    if (sum(c.isdigit() for c in password) >= DIGITS_MIN
-        and sum(c.isalpha() for c in password) >= LETTERS_MIN
-        and no_next_match(password)
-        ):
-        break
-
-try:
-    pyperclip.copy(password)
-    capture = pyperclip.paste()  # Capture paste and begin countdown
-    try:
-        print('Password generated and copied to clipboard')
-        pyperclip.waitForNewPaste(SECONDS_IN_CLIPBOARD)
-    except:
-        pyperclip.copy('')
-        pass
-except:
-    print('Password generated:\n{}'.format(password))
-
-print('--Password-Generator end---')
 
 # Todo:
-# 3. Call functions and saved values only if needed
 # 4. Change to check by conditions added instead of adding them by hand
 # 5. Add params
 # - argparse
 # - sys, getopt
+# 6. Add if options are valid and could generate a password
